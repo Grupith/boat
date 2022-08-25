840971eed8f12b20d69410b29fdc16548207f7de
Dylan Koss <dylanhkoss@gmail.com>
2022-08-24 21:05:27 -0500

    completed landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import LandingPage from './Components/LandingPage';
+import { useState } from 'react';
 
 
 function App() {
+
+  const [firstName, setFirstName] = useState();
+
   return (
     <div className="App">
-     <LandingPage />
+     <LandingPage firstName={firstName} setFirstName={setFirstName} />
     </div>
   );
 }
--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
-export default function LandingPage() {
+export default function LandingPage({ firstName, setFirstName}) {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+  }
+
   return (
     <div className='LandingPage'>
         <div className='titleContainer'>
@@ -11,9 +17,10 @@ export default function LandingPage() {
         <h3 className='paragraphTitle'> Why lifeboat?</h3>
         <p className='paragraph'> Are you someone to use excel sheets to track your monthly bills?
            Well Lifeboat offers a clean UI to easily manage your finances.</p>
-        <form className='nameForm'>
+        <form className='nameForm' onSubmit={handleSubmit}>
             <label for='name' className='nameLabel'>Enter your First Name</label>
-            <input type='text' className='nameInput' />
+            <input type='text' className='nameInput' onChange={e => setFirstName(e.target.value)}/>
+            <h3>{firstName}</h3>
             <button type='submit' className='submitNameButton'>Create Budget</button>
         </form>
     </div>
